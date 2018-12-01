5d6884d539b2a03ecacefbf7297a7e38bb5f8850
William Frankish <wfrankish@gmail.com>
2018-12-01 23:35:03 +0000

    variant of puzzle 2 that actually finished before I got bored

--- 1/puzzle2.cpp
+++ 1/puzzle2.cpp
@@ -7,26 +7,45 @@ int main()
 {
     int currentFrequency = 0;
     std::list<int> reachedFrequencies;
-    while (true) {
-        std::ifstream file("input");
-        std::string line;
-        while (std::getline(file, line)) {
-            // input format is +n or -n, parse function happily accepts +;
-            int change = std::stoi(line);
-            currentFrequency += change;
+    std::list<int> sortedReachedFrequencies;
+    std::ifstream file("input");
+    std::string line;
+    while (std::getline(file, line)) {
+        // input format is +n or -n, parse function happily accepts +;
+        int change = std::stoi(line);
+        currentFrequency += change;
 
-            std::list<int>::iterator it = std::lower_bound(reachedFrequencies.begin(), reachedFrequencies.end(), currentFrequency);
+        reachedFrequencies.insert(reachedFrequencies.end(), currentFrequency);
 
-            if (it != reachedFrequencies.end()) {
-                int found = *it;
-                if (found == currentFrequency) {
-                    std::cout << "Found duplicate frequency: " << currentFrequency;
-                    return 0;
-                }
+        std::list<int>::iterator targetPosition = std::lower_bound(sortedReachedFrequencies.begin(), sortedReachedFrequencies.end(), currentFrequency);
+
+        if (targetPosition != sortedReachedFrequencies.end()) {
+            int found = *targetPosition;
+            if (found == currentFrequency) {
+                std::cout << "Found duplicate frequency: " << currentFrequency;
+                return 0;
             }
-            reachedFrequencies.insert(it, currentFrequency);
         }
 
-        std::cout << "Reached end of input, list now has size: " << reachedFrequencies.size() << '\n';
+        sortedReachedFrequencies.insert(targetPosition, currentFrequency);
+    }
+
+    std::cout << "Reached end of input\n";
+
+    int total = currentFrequency;
+    int currentTotal = total;
+    while (true) {
+        std::list<int>::iterator it;
+        for (it = reachedFrequencies.begin(); it != reachedFrequencies.end(); it++) {
+            int reachedFrequency = *it;
+            currentFrequency = reachedFrequency + currentTotal;
+            if (std::binary_search(sortedReachedFrequencies.begin(), sortedReachedFrequencies.end(), currentFrequency)) {
+                std::cout << "Found duplicate frequency: " << currentFrequency;
+                return 0;
+            }
+        }
+        currentTotal += total;
+
+        std::cout << "Reached end of iteration\n";
     }
 }
